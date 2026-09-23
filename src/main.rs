70651e2ed1443bdd31475136d395368882226f7c
FlorianRoudaut <florian.roudaut@ubisoft.com>
2026-09-23 10:48:30 +0200

    Added code to retrieve and save security data for S&P 500

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,66 @@
-use std::fs;
-use prost::Message;
+use std::path::Path;
 use neko_ibkr_api::client::IbkrClient;
 use neko_ibkr_api::contract_stock::get_contract_stock_sync;
-use neko_securities::{Stock, Key};
-use neko_securities::proto::{proto_to_stock, StockList};
+use neko_securities::{Exchange, Stock, Key};
+use neko_tech_localstorage::LocalFileRepository;
+use neko_tech_persistence::Repository;
 use uuid::Uuid;
 
-fn fetch_stock(client: &IbkrClient, ticker: &str) -> Stock {
-    let result = get_contract_stock_sync(client, ticker)
-        .unwrap_or_else(|e| panic!("Failed to fetch {} contract: {}", ticker, e));
+fn fetch_and_save_stock(client: &IbkrClient, ticker: &str) {
+    let result = match get_contract_stock_sync(client, ticker) {
+        Ok(r) => r,
+        Err(e) => { eprintln!("Failed to fetch {} contract: {}", ticker, e); return; }
+    };
 
-    let stock_info = result
-        .get(ticker)
-        .and_then(|infos| infos.first())
-        .unwrap_or_else(|| panic!("No info returned for {}", ticker));
+    let stock_info = match result.get(ticker).and_then(|infos| infos.first()) {
+        Some(info) => info,
+        None => { eprintln!("No info returned for {}", ticker); return; }
+    };
 
-    let contract = stock_info
-        .contracts
-        .iter()
-        .find(|c| c.is_us)
-        .unwrap_or_else(|| panic!("No US contract found for {}", ticker));
+    let data_dir = Path::new("../data");
+    let mut exchange_repo: LocalFileRepository<Exchange> = LocalFileRepository::<Exchange>::load(data_dir)
+        .expect("Failed to load exchange repository");
+    let mut stock_repo: LocalFileRepository<Stock> = LocalFileRepository::<Stock>::load(data_dir)
+        .expect("Failed to load stock repository");
 
-    Stock {
-        key: Key { id: Uuid::new_v4(), version: 0, unique_name: ticker.to_string() },
-        name: ticker.to_string(),
-        bbg_id: String::new(),
-        cusip: String::new(),
-        isin: String::new(),
-        ibkr_contract_id: contract.conid.to_string(),
-        exchange_ids: vec![],
+    for contract in &stock_info.contracts {
+
+       let exchange =  Exchange {
+                key: Key { id: Uuid::new_v4(), version: 0, unique_name: contract.exchange.clone() },
+                name: contract.exchange.clone(),
+        };
+        
+        let exchange_read_result = exchange_repo.read_one_by_name(&contract.exchange);
+        if exchange_read_result.is_err() {
+            let _res = exchange_repo.create(exchange);
+        }
+
+        let stock = Stock {
+            key: Key {
+                id: Uuid::new_v4(),
+                version: 0,
+                unique_name: format!("{}_{}", ticker, contract.exchange),
+            },
+            name: stock_info.name.clone(),
+            bbg_id: String::new(),
+            cusip: String::new(),
+            isin: String::new(),
+            ibkr_contract_id: contract.conid.to_string(),
+            exchange_ids: vec![contract.exchange.clone()],
+        };
+
+        let stock_read_result = stock_repo.read_one_by_name(&stock.name);
+        if stock_read_result.is_err() {
+            let _res = stock_repo.create(stock);
+        }
     }
 }
 
 fn main() {
-    /*let client = IbkrClient::new().expect("Failed to create IBKR client");
+    let client = IbkrClient::new().expect("Failed to create IBKR client");
 
-    let tickers = ["AAPL", "MSFT", "GOOGL", "META"];
-    let stocks: Vec<Stock> = tickers.iter().map(|t| fetch_stock(&client, t)).collect();
-
-    let stock_list = StockList {
-        stocks: stocks.iter().map(stock_to_proto).collect(),
-    };
-    let blob = stock_list.encode_to_vec();
-    fs::write("equities.bin", &blob).expect("Failed to write file");*/
-
-    let bytes = fs::read("equities.bin").expect("Failed to read file");
-    let loaded_list = StockList::decode(bytes.as_slice()).expect("Deserialization failed");
-    let loaded: Vec<Stock> = loaded_list.stocks.into_iter().map(proto_to_stock).collect();
-
-    for stock in &loaded {
-        println!("ticker: {}, conid: {}", stock.name, stock.ibkr_contract_id);
+    let tickers = ["MMM", "AOS", "ABT", "ABBV", "ACN", "ADBE", "AMD", "AES", "AFL", "A", "APD", "ABNB", "AKAM", "ALB", "ARE", "ALGN", "ALLE", "LNT", "ALL", "GOOGL", "GOOG", "MO", "AMZN", "AMCR", "AEE", "AEP", "AXP", "AIG", "AMT", "AWK", "AMP", "AME", "AMGN", "APH", "ADI", "AON", "APA", "APO", "AAPL", "AMAT", "APP", "APTV", "ACGL", "ADM", "ARES", "ANET", "AJG", "AIZ", "T", "ATO", "ADSK", "ADP", "AZO", "AVY", "AXON", "BKR", "BALL", "BAC", "BAX", "BDX", "BRK.B", "BBY", "TECH", "BIIB", "BLK", "BX", "XYZ", "BNY", "BA", "BKNG", "BSX", "BMY", "AVGO", "BR", "BRO", "BF.B", "BLDR", "BG", "BXP", "CHRW", "CDNS", "CPT", "COF", "CAH", "CCL", "CARR", "CVNA", "CASY", "CAT", "CBOE", "CBRE", "CDW", "COR", "CNC", "CNP", "CF", "CRL", "SCHW", "CHTR", "CVX", "CMG", "CB", "CHD", "CIEN", "CI", "CINF", "CTAS", "CSCO", "C", "CFG", "CLX", "CME", "CMS", "KO", "CTSH", "COHR", "COIN", "CL", "CMCSA", "FIX", "COP", "ED", "STZ", "CEG", "COO", "CPRT", "GLW", "CPAY", "CTVA", "CSGP", "COST", "CRH", "CRWD", "CCI", "CSX", "CMI", "CVS", "DHR", "DRI", "DDOG", "DVA", "DECK", "DE", "DELL", "DAL", "DVN", "DXCM", "FANG", "DLR", "DG", "DLTR", "D", "DPZ", "DASH", "DOV", "DOW", "DHI", "DTE", "DUK", "DD", "ETN", "EBAY", "ECHO", "ECL", "EIX", "EW", "ELV", "EME", "EMR", "ETR", "EOG", "EQT", "EFX", "EQIX", "ERIE", "ESS", "EL", "EG", "EVRG", "ES", "EXC", "EXE", "EXPE", "EXPD", "EXR", "XOM", "FFIV", "FDS", "FICO", "FAST", "FRT", "FDX", "FDXF", "FERG", "FIS", "FITB", "FSLR", "FE", "FISV", "FLEX", "F", "FTNT", "FTV", "FOXA", "FOX", "BEN", "FCX", "GRMN", "IT", "GE", "GEHC", "GEV", "GEN", "GNRC", "GD", "GIS", "GM", "GPC", "GILD", "GPN", "GL", "GDDY", "GS", "HAL", "HIG", "HAS", "HCA", "DOC", "HSIC", "HSY", "HPE", "HLT", "HD", "HONA", "HON", "HRL", "HST", "HWM", "HPQ", "HUBB", "HUM", "HBAN", "HII", "IBM", "IEX", "IDXX", "ITW", "INCY", "IR", "PODD", "INTC", "IBKR", "ICE", "IFF", "IP", "INTU", "ISRG", "IVZ", "INVH", "IQV", "IRM", "JBHT", "JBL", "JKHY", "J", "JNJ", "JCI", "JPM", "KVUE", "KDP", "KEY", "KEYS", "KMB", "KIM", "KMI", "KKR", "KLAC", "KHC", "KR", "LHX", "LH", "LRCX", "LVS", "LDOS", "LEN", "LII", "LLY", "LIN", "LYV", "LMT", "L", "LOW", "LULU", "LITE", "LYB", "MTB", "MPC", "MAR", "MRSH", "MLM", "MRVL", "MAS", "MA", "MKC", "MCD", "MCK", "MDT", "MRK", "META", "MET", "MTD", "MGM", "MCHP", "MU", "MSFT", "MAA", "MRNA", "TAP", "MDLZ", "MPWR", "MNST", "MCO", "MS", "MOS", "MSI", "MSCI", "NDAQ", "NTAP", "NFLX", "NEM", "NWSA", "NWS", "NEE", "NKE", "NI", "NDSN", "NSC", "NTRS", "NOC", "NCLH", "NRG", "NUE", "NVDA", "NVR", "NXPI", "ORLY", "OXY", "ODFL", "OMC", "ON", "OKE", "ORCL", "OTIS", "PCAR", "PKG", "PLTR", "PANW", "PSKY", "PH", "PAYX", "PYPL", "PNR", "PEP", "PFE", "PCG", "PM", "PSX", "PNW", "PNC", "PPG", "PPL", "PFG", "PG", "PGR", "PLD", "PRU", "PEG", "PTC", "PSA", "PHM", "PWR", "QCOM", "DGX", "Q", "RL", "RJF", "RDDT", "RTX", "O", "REG", "REGN", "RF", "RSG", "RMD", "RVTY", "HOOD", "ROK", "ROL", "ROP", "ROST", "RCL", "SPGI", "CRM", "SNDK", "SBAC", "SLB", "STX", "SRE", "NOW", "SHW", "SPG", "SWKS", "SJM", "SW", "SNA", "SOLV", "SO", "LUV", "SWK", "SBUX", "STT", "STLD", "STE", "SYK", "SMCI", "SYF", "SNPS", "SYY", "TMUS", "TROW", "TTWO", "TPR", "TRGP", "TGT", "TEL", "TDY", "TER", "TSLA", "TXN", "TPL", "TXT", "TMO", "TJX", "TKO", "TTD", "TSCO", "TT", "TDG", "TRV", "TRMB", "TFC", "TYL", "TSN", "USB", "UBER", "UDR", "ULTA", "UNP", "UAL", "UPS", "URI", "UNH", "UHS", "VLO", "VEEV", "VTR", "VLTO", "VRSN", "VRSK", "VZ", "VRTX", "VRT", "VTRS", "VICI", "V", "VST", "VMRK", "VMC", "WRB", "GWW", "WAB", "WMT", "DIS", "WBD", "WM", "WAT", "WEC", "WFC", "WELL", "WST", "WDC", "WY", "WSM", "WMB", "WTW", "WDAY", "WYNN", "XEL", "XYL", "YUM", "ZBRA", "ZBH", "ZTS"];
+    for ticker in tickers.iter() {
+        fetch_and_save_stock(&client, ticker);
     }
-
-    println!("Loaded {} stocks successfully.", loaded.len());
 }
